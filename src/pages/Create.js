7838e0ee4c1b60332b7b0d7c4aac4d82e8a4d695
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2022-01-23 23:42:20 +0100

    feat: setup json-server to GET and POST notes, implemented useHistory hook

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -12,6 +12,7 @@ import {
   makeStyles,
 } from "@material-ui/core";
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles({
   field: {
@@ -27,6 +28,7 @@ const useStyles = makeStyles({
 
 export default function Create() {
   const classes = useStyles();
+  const history = useHistory();
   const [title, setTitle] = useState("");
   const [details, setDetails] = useState("");
   const [titleError, setTitleError] = useState(false);
@@ -46,7 +48,11 @@ export default function Create() {
     }
 
     if (title && details) {
-      console.log(`${title}, ${details}, ${category}`);
+      fetch("http://localhost:8000/notes", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ title, details, category }),
+      }).then(() => history.push("/"));
     }
   };
 
